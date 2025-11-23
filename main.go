88444513d1183772a4210e4baba024eea275a4bd
Uthpala Heenatigala <uthpala@passionatepeople.io>
2025-11-23 17:14:52 +0100

    add healthz endpoint

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
+
 import "net/http"
 
 func main() {
   mux := http.NewServeMux()
 
-  mux.Handle("/", http.FileServer(http.Dir(".")))
+  mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+  mux.HandleFunc("/healthz", handleRediness)
 
   server := http.Server {
     Addr: ":8080",
@@ -12,4 +14,10 @@ func main() {
   }
 
   server.ListenAndServe()
+}
+
+func handleRediness(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+    w.WriteHeader(http.StatusOK)
+    w.Write([]byte(http.StatusText(http.StatusOK)))
 }
